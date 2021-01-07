ff99ca3eee85adf1c85e51aca9429db1bd976105
leonardo lira <leon_lira_@hotmail.com>
2021-01-07 16:10:38 -0600

    cambio de cors

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ app.use(bodyparser.json({limit:'10mb',extended:true}));
 app.use(helmet());
 
 //cors
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
 
+})
 
 // rutas 
 
